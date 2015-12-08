e323a127eeeeede4b6ca3ca604ef3d715b368c71
thePLO <pdemiguel@enti.cat>
2015-12-09 00:39:57 +0100

    redis

---
+++ nav.js
@@ -0,0 +1,77 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a id='m_in' href='../index.html'>AECH</a></li>" +
+"<li><a id='m_ca' href='../castors/qui_som.html'>C/LL</a></li>" +
+"<li><a id='m_ll' href='../llops/qui_som.html'>LL/D</a></li>" +
+"<li><a id='m_ra' href='../rangers/qui_som.html'>R/G</a></li>" +
+"<li><a id='m_pi' href='../pioners/qui_som.html'>P/C</a></li>" +
+"<li><a id='m_ro' href='../rovers/qui_som.html'>R</a></li>" +
+"<li><a id='m_kr' href='../kraal/monitors.html'>Kraal</a></li>" +
+"</ul>";
+//submenus
+//castors
+if(document.getElementById("navC2")){
+	document.getElementById("navC2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../castors/qui_som.html'>Qui som?</a></li>" +
+	"<li><a href='../castors/calendari.html'>Calendari</a></li>" +
+	"<li><a href='../castors/objectius.html'>Objectius</a></li>" +
+	"<li><a href='../castors/monitors.html'>Monitors</a></li>" +
+	"<li><a href='../castors/media.html'>Fotos i Video</a></li>" +
+	"</ul>";
+}
+//llops
+if(document.getElementById("navL2")){
+	document.getElementById("navL2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../llops/qui_som.html'>Qui som?</a></li>" +
+	"<li><a href='../llops/calendari.html'>Calendari</a></li>" +
+	"<li><a href='../llops/objectius.html'>Objectius</a></li>" +
+	"<li><a href='../llops/monitors.html'>Monitors</a></li>" +
+	"<li><a href='../llops/media.html'>Fotos i Video</a></li>" +
+	"</ul>";
+}
+//ran
+if(document.getElementById("navr2")){
+	document.getElementById("navr2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../rangers/qui_som.html'>Qui som?</a></li>" +
+	"<li><a href='../rangers/calendari.html'>Calendari</a></li>" +
+	"<li><a href='../rangers/objectius.html'>Objectius</a></li>" +
+	"<li><a href='../rangers/monitors.html'>Monitors</a></li>" +
+	"<li><a href='../rangers/media.html'>Fotos i Video</a></li>" +
+	"</ul>";
+}
+//pios
+if(document.getElementById("navP2")){
+	document.getElementById("navP2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../pioners/qui_som.html'>Qui som?</a></li>" +
+	"<li><a href='../pioners/calendari.html'>Calendari</a></li>" +
+	"<li><a href='../pioners/objectius.html'>Objectius</a></li>" +
+	"<li><a href='../pioners/monitors.html'>Monitors</a></li>" +
+	"<li><a href='../pioners/media.html'>Fotos i Video</a></li>" +
+	"</ul>";
+}
+//ro
+if(document.getElementById("navR2")){
+	document.getElementById("navR2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../rovers/qui_som.html'>Qui som?</a></li>" +
+	"<li><a href='../rovers/calendari.html'>Calendari</a></li>" +
+	"<li><a href='../rovers/objectius.html'>Objectius</a></li>" +
+	"<li><a href='../rovers/consellers.html'>Monitors</a></li>" +
+	"<li><a href='../rovers/media.html'>Fotos i Video</a></li>" +
+	"</ul>";
+}
+//kraal
+if(document.getElementById("navK2")){
+	document.getElementById("navK2").innerHTML =
+	"<ul id='submenu'>" +
+	"<li><a href='../kraal/monitors.html'>Monitors</a></li>" +
+	"<li><a href='../kraal/dadesB.html'>Dades Banc</a></li>" +
+	"<li><a href='../kraal/carrecs.html'>Càrrecs</a></li>" +
+	"<li><a href='../kraal/links.html'>Enllaços</a></li>" +
+	"<li><a href='../kraal/sap.html'>S.A.P.</a></li>" +
+	"</ul>";
+}
